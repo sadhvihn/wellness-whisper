6eb6eb519e50556308310135b1254f0b3efbbe7a
sadhvihn <sadhvihnbhatt@gmail.com>
2024-09-24 00:05:57 +0530

    comit changes

---
+++ server/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  
+  try {
+    await mongoose.connect(process.env.DB, connectionParams);
+    console.log("Connected to database successfully");
+  } catch (error) {
+    console.error("Error connecting to database:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
